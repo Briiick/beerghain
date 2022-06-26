c2347aa0eb0f754a018a09db7f91f68dd00de9c7
Ruban Rengaraju <rubanrengaraju@Rubans-MacBook-Pro.local>
2022-06-25 23:31:58 -0400

    Add endpoint to setup enterprise subscription service

--- server/src/company_endpoints.ts
+++ server/src/company_endpoints.ts
@@ -1,12 +1,33 @@
 import express from 'express';
 const router = express.Router();
 
-// var automatedRoutes = require('./testRoutes/automated');
-// // Import my test routes into the path '/test'
-// app.use('/tests', testRoutes);
+var CompanyTable = []
 
+function CompanyEntry(eth_address, price, name) {
+    this.eth_address = eth_address;
+    this.price = price;
+    this.name = name;
+}
+
+// Test get request
 router.get('/company/setup', (req, res) => {
+    console.log("Params are here ",req.query);
+    
     res.send('Company route');
+})
+
+router.post('/company/setup', (req, res) => {
+    let newEntry = new CompanyEntry(req.body["eth_address"], Number(req.body["price"]), req.body["name"])
+    console.log(newEntry)
+    // validate new entry's keys
+    Object.keys(newEntry).forEach((key, _) => {
+        if (newEntry[key] !== null && newEntry[key] !== NaN) {
+            res.sendStatus(400);
+        }
+    })
+
+    CompanyTable.push(newEntry)
+    res.sendStatus(200);
 })
  
 module.exports = router;
